c249809d01888d64e5fe1db53d4f0ca639d77781
Ch4nYH <hfercxx@hotmail.com>
2021-02-27 10:31:06 +0800

    updatea

--- CIFAR10_DCGAN_pytorch.py
+++ CIFAR10_DCGAN_pytorch.py
@@ -1,4 +1,4 @@
-# MNIST image generation using DCGAN
+# CIFAR10 image generation using DCGAN
 import torch
 from torch.autograd import Variable
 import torchvision.datasets as dsets
@@ -20,20 +20,20 @@ learning_rate = 0.0002
 betas = (0.5, 0.999)
 batch_size = 128
 num_epochs = 20
-data_dir = '../Data/MNIST_data/'
-save_dir = 'MNIST_DCGAN_results/'
+data_dir = '../Data/CIFAR10_data/'
+save_dir = 'CIFAR10_DCGAN_results/'
 
-# MNIST dataset
+# CIFAR10 dataset
 transform = transforms.Compose([transforms.Scale(image_size),
                                 transforms.ToTensor(),
                                 transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
 
-mnist_data = dsets.CIFAR10(root=data_dir,
+cifar_data = dsets.CIFAR10(root=data_dir,
                          train=True,
                          transform=transform,
                          download=True)
 
-data_loader = torch.utils.data.DataLoader(dataset=mnist_data,
+data_loader = torch.utils.data.DataLoader(dataset=cifar_data,
                                           batch_size=batch_size,
                                           shuffle=True)
 
@@ -138,7 +138,7 @@ class Discriminator(torch.nn.Module):
 
 
 # Plot losses
-def plot_loss(d_losses, g_losses, num_epoch, save=False, save_dir='MNIST_DCGAN_results/', show=False):
+def plot_loss(d_losses, g_losses, num_epoch, save=False, save_dir='CIFAR10_DCGAN_results/', show=False):
     fig, ax = plt.subplots()
     ax.set_xlim(0, num_epochs)
     ax.set_ylim(0, max(np.max(g_losses), np.max(d_losses))*1.1)
@@ -152,7 +152,7 @@ def plot_loss(d_losses, g_losses, num_epoch, save=False, save_dir='MNIST_DCGAN_r
     if save:
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
-        save_fn = save_dir + 'MNIST_DCGAN_losses_epoch_{:d}'.format(num_epoch + 1) + '.png'
+        save_fn = save_dir + 'CIFAR10_DCGAN_losses_epoch_{:d}'.format(num_epoch + 1) + '.png'
         plt.savefig(save_fn)
 
     if show:
@@ -161,7 +161,7 @@ def plot_loss(d_losses, g_losses, num_epoch, save=False, save_dir='MNIST_DCGAN_r
         plt.close()
 
 
-def plot_result(generator, noise, num_epoch, save=False, save_dir='MNIST_DCGAN_results/', show=False, fig_size=(5, 5)):
+def plot_result(generator, noise, num_epoch, save=False, save_dir='CIFAR10_DCGAN_results/', show=False, fig_size=(5, 5)):
     generator.eval()
 
     noise = Variable(noise.cuda())
@@ -175,7 +175,7 @@ def plot_result(generator, noise, num_epoch, save=False, save_dir='MNIST_DCGAN_r
     fig, axes = plt.subplots(n_rows, n_cols, figsize=fig_size)
     for ax, img in zip(axes.flatten(), gen_image):
         ax.axis('off')
-        ax.set_adjustable('box-forced')
+        #ax.set_adjustable('box-forced')
         ax.imshow(img.cpu().data.view(image_size, image_size).numpy(), cmap='gray', aspect='equal')
     plt.subplots_adjust(wspace=0, hspace=0)
     title = 'Epoch {0}'.format(num_epoch+1)
@@ -185,7 +185,7 @@ def plot_result(generator, noise, num_epoch, save=False, save_dir='MNIST_DCGAN_r
     if save:
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
-        save_fn = save_dir + 'MNIST_DCGAN_epoch_{:d}'.format(num_epoch+1) + '.png'
+        save_fn = save_dir + 'CIFAR10_DCGAN_epoch_{:d}'.format(num_epoch+1) + '.png'
         plt.savefig(save_fn)
 
     if show:
